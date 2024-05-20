0c2162eafe4a0ef639f11343f82dfe9686c7b06a
BinzXD <starswars164@gmail.com>
2024-05-20 11:27:55 +0700

    login

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,19 @@ import 'package:nutriscan/pages/profil.dart';
 import 'package:nutriscan/pages/scan.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:nutriscan/user_auth/registerpage.dart';
+import 'package:nutriscan/user_auth/login_page.dart';
+import 'package:nutriscan/user_auth/sign_up.dart';
+import 'package:nutriscan/pages/splash_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:nutriscan/firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +27,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'NutriScan',
       theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.orange),
-      home: HomePage(),
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => LoginPage(),
+        '/signUp': (context) => SignUpPage(),
+        // '/setting': (context) => SettingPage(),
+        '/home': (context) => HomePage(),
+      },
     );
   }
 }
